a1486ef80eede9e8b85b72ac2f9fc989594f3f13
markchang <markchang@mi-tech.com.tw>
2019-07-30 12:07:45 +0800

    初次提交

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { Apiresult } from './apiresult';
+import { HttpModule, Http, Headers } from '@angular/http';
+import { HttpService } from './model/http.service';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private http: HttpService, private HTTP: Http) { }
   title = 'MarkPanel';
+  result: {};
+  testobj={
+    "The MRT Stations in New Taipei City": "淡水站",
+    name:22
+  };
+
+  ngOnInit(): void {
+ 
+    // this.http.authGet("http://localhost:3000/result")
+    //   .then(res => {
+    //   this.result = res
+    //     console.log(this.result)
+    //   })
+
+    this.getinfo().then(res=>{this.result=res},rej=>{this.result=rej})
+  }
+  getinfo() {
+    let hh=new Headers();
+    // hh.set("Content-Type", "application/json");
+    return this.HTTP
+      .get("http://localhost:3000/result").toPromise()
+      .then(respons => {
+        let result = respons;
+        console.log(result)
+        return result;
+      }, reject => {
+        return (reject)
+      })
+      .catch(
+
+      )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { HttpService } from './model/http.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    // Add an icon to the library for convenient access in other components
+    library.add(faCoffee);
+  }
+
+}
